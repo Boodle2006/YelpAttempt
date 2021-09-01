4c7a09082110135d8ab7aeafb03d8d59e3fa13d3
Ben Goldston <bgoldston.2024@saintannsny.org>
2021-09-01 09:04:06 -0400

    Initial Commit

---
+++ YelpAPIApp/YelpAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpAPIAppApp.swift
+//  YelpAPIApp
+//
+//  Created by James Goldston on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct YelpAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
